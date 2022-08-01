a2629d92d9b66fbc30288a9a94208f3c5974500a
KHC <KHC@DESKTOP-MFR3B8G>
2022-08-01 23:16:18 +0900

    ..

--- project2_bookcafe/src/main/java/member/MemberDao.java
+++ project2_bookcafe/src/main/java/member/MemberDao.java
@@ -1,8 +1,6 @@
 package member;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 
 public class MemberDao {
-	private DataSource dataSource;
 	
 }
--- project2_bookcafe/src/main/java/review_board/ReviewBoardDao.java
+++ project2_bookcafe/src/main/java/review_board/ReviewBoardDao.java
@@ -1,7 +1,5 @@
 package review_board;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 
 public class ReviewBoardDao {
-	private DataSource dataSource;
 }
