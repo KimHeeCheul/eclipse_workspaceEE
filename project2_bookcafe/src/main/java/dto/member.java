33b27e859d0b1e0ece823af3649aad2adafd4aab
KHC <KHC@DESKTOP-MFR3B8G>
2022-08-01 22:24:48 +0900

    ...

---
+++ project2_bookcafe/src/main/java/dto/member.java
@@ -0,0 +1,108 @@
+package dto;
+
+import java.sql.Date;
+/*
+USERID	VARCHAR2(100 BYTE)	No		1	
+PASSWORD	VARCHAR2(100 BYTE)	Yes		2	
+NAME	VARCHAR2(100 BYTE)	Yes		3	
+PHONE	VARCHAR2(100 BYTE)	Yes		4	
+BIRTH	VARCHAR2(100 BYTE)	Yes		5	
+ADDRESS	VARCHAR2(100 BYTE)	Yes		6	
+EMAIL	VARCHAR2(100 BYTE)	Yes		7	
+INTEREST	VARCHAR2(200 BYTE)	Yes		8	
+ */
+
+public class member {
+	private String userId;
+	private String password;
+	private String name;
+	private String phone;
+	private String birth;
+	private String address;
+	private String email;
+	private String interest;
+
+	public member(String userId, String password, String name, String phone, String birth, String address,
+			String email, String interest) {
+		super();
+		this.userId = userId;
+		this.password = password;
+		this.name = name;
+		this.phone = phone;
+		this.birth = birth;
+		this.address = address;
+		this.email = email;
+		this.interest = interest;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getInterest() {
+		return interest;
+	}
+
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+
+	@Override
+	public String toString() {
+		return "product [userId=" + userId + ", password=" + password + ", name=" + name + ", phone=" + phone
+				+ ", birth=" + birth + ", address=" + address + ", email=" + email + ", interest=" + interest + "]";
+	}
+
+}
---
+++ project2_bookcafe/src/main/java/dto/review_board.java
@@ -0,0 +1,90 @@
+package dto;
+
+import java.sql.Date;
+
+public class review_board {
+	private int r_no;
+	private Date r_date;
+	private String r_title;
+	private String r_content;
+	private int r_count;
+	/*********** Member ************/
+	private member userId;
+	/*********** product ************/
+	private product p_no;
+
+	public review_board(int r_no, Date r_date, String r_title, String r_content, int r_count, member userId,
+			product p_no) {
+		super();
+		this.r_no = r_no;
+		this.r_date = r_date;
+		this.r_title = r_title;
+		this.r_content = r_content;
+		this.r_count = r_count;
+		this.userId = userId;
+		this.p_no = p_no;
+	}
+
+	public int getR_no() {
+		return r_no;
+	}
+
+	public void setR_no(int r_no) {
+		this.r_no = r_no;
+	}
+
+	public Date getR_date() {
+		return r_date;
+	}
+
+	public void setR_date(Date r_date) {
+		this.r_date = r_date;
+	}
+
+	public String getR_title() {
+		return r_title;
+	}
+
+	public void setR_title(String r_title) {
+		this.r_title = r_title;
+	}
+
+	public String getR_content() {
+		return r_content;
+	}
+
+	public void setR_content(String r_content) {
+		this.r_content = r_content;
+	}
+
+	public int getR_count() {
+		return r_count;
+	}
+
+	public void setR_count(int r_count) {
+		this.r_count = r_count;
+	}
+
+	public member getUserId() {
+		return userId;
+	}
+
+	public void setUserId(member userId) {
+		this.userId = userId;
+	}
+
+	public product getP_no() {
+		return p_no;
+	}
+
+	public void setP_no(product p_no) {
+		this.p_no = p_no;
+	}
+
+	@Override
+	public String toString() {
+		return "review_board [r_no=" + r_no + ", r_date=" + r_date + ", r_title=" + r_title + ", r_content=" + r_content
+				+ ", r_count=" + r_count + ", userId=" + userId + ", p_no=" + p_no + "]";
+	}
+
+}
