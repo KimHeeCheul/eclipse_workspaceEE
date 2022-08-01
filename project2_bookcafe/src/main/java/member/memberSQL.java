1baf08274461ddcbe07a5baa311e440caade4979
KHC <KHC@DESKTOP-MFR3B8G>
2022-08-01 22:48:51 +0900

    ..

---
+++ project2_bookcafe/src/main/java/member/memberSQL.java
@@ -0,0 +1,16 @@
+package member;
+
+public class memberSQL {
+	public final static String MEMBER_INSERT=
+			"insert into member (userId,password,name,phone,birth,address,email,interest) values (?,?,?,?,?,?,?,?)";
+	public final static String MEMBER_UPDATE=
+			"update member set password=?,name=?,phone=?,birth=?,address=?,email=?,interest=? where userId=?";
+	public final static String MEMBER_REMOVE=
+			"delete member where userId=?";
+	public final static String MEMBER_SELECT_BY_ID=
+			"select userId,password,name,phone,birth,address,email,interest from member where userId=?";
+	public final static String MEMBER_SELECT_ALL=
+			"select userId,password,name,phone,birth,address,email,interest from member";
+	public final static String MEMBER_SELECT_BY_ID_COUNT=
+			"select count(*) from member where userId=?";
+}
---
+++ project2_bookcafe/src/main/java/review_board/review_boardSQL.java
@@ -0,0 +1,6 @@
+package review_board;
+
+public class review_boardSQL {
+	public final static String REVIEW_BOARD_INSERT=
+			"insert into review_board (r_no,r_date,r_title,r_content,r_count,)";
+}
