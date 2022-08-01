d6d538c745e27a0ea62890a82e6e6329a692d13d
KHC <KHC@DESKTOP-MFR3B8G>
2022-08-01 23:08:08 +0900

    ...

--- project2_bookcafe/src/main/java/review_board/review_boardSQL.java
+++ project2_bookcafe/src/main/java/review_board/review_boardSQL.java
@@ -2,5 +2,11 @@ package review_board;
 
 public class review_boardSQL {
 	public final static String REVIEW_BOARD_INSERT=
-			"insert into review_board (r_no,r_date,r_title,r_content,r_count,)";
+			"insert into review_board (r_no,r_date,r_title,r_content,r_count,userId,p_no)";
+	public final static String REVIEW_BOARD_UPDATE=
+			"update review_board set r_date=?,r_title=?,r_content=?,r_count=?,userId=?,p_no=? where r_no=?";
+	public final static String REVIEW_BOARD_REMOVE=
+			"delete review_board where r_no=?";
+	public final static String REVIEW_BOARD_BY_NO=
+			"select r_no,r_date,r_title,r_content,r_count,userId,p_no from review_board where r_no=?";
 }
