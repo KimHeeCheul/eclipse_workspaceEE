438274fee62138e1db4d5223245dc6bade1fd991
KimHeeCheul <kim11403@naver.com>
2022-08-03 19:39:41 +0900

    ..

--- board_model1/src/main/webapp/sql/board_insert.sql
+++ board_model1/src/main/webapp/sql/board_insert.sql
@@ -21,34 +21,38 @@ CREATE TABLE board
 
 --새글쓰기(최상위글)
 --새글쓰기
-INSERT INTO
-board(boardno, title, writer, content,groupno,step,depth) 
-VALUES(	board_sequence.nextval,
-        '게시판타이틀'||board_sequence.currval,
-        '김경호'||board_sequence.currval,
-        'content'||board_sequence.currval,
-        board_sequence.currval,
-        1,
-        0);
-        
-        
+insert into board(boardno,title,writer,cotent,groupno,step,depth) values(board_sequence.nextval,
+'게시판 타이틀'||board_sequence.currval,
+'김경호'||board_sequence.currval,
+'내용'||board_sequence.currval,
+board_sequence.currval,
+1,
+0
+);
 
-commit;   
-        
-INSERT INTO Board ( boardno,title,writer,content,groupno,step) 
-VALUES (board_sequence.nextval, ?, ?, ?,board_sequence.currval, 1);
-commit;
+
+
 --답글쓰기
---INSERT INTO Board ( boardno,title,writer,content,groupno,step,depth) 
---VALUES (board_sequence.nextval, '3번답글3번', 'KIM','CONTEN',3, 2, 1);
 
---
 /*현재글의 데이타를 기반으로 작업한다.
- * 1. 현재글과 같은그룹번호들중에서현재글의 step보다큰 step을가진 게시물들의 step을 1씩 증가시킨다.
- */
+  1. boardno : 시퀀스 증가
+  2. groupno : 현재글의 groupno
+  3. step    : 현재글의 step + 1
+  4. depth   : 현재글의 depth + 1
+*/
+-- update현재글과 같은그룹번호들중에서현재글의 step보다큰 step을가진 게시물들의 step을 1씩 증가시킨다.
+update board 
+set step = step +1 
+where step > 1 
+and groupno =564;
+--insert 
+ 
  --1번step
-UPDATE board SET step = step + 1 WHERE step >= 2 AND groupno = 563;
-commit;
+
+
+
+
+
 /*
  * 2. 
  * 	  boardno:시퀀스증가
@@ -58,20 +62,10 @@ commit;
  *    
  */
  
-INSERT INTO board(boardno,title,writer,content, groupno, step, depth) 
-               VALUES
-            (board_sequence.nextval,
-                '563번답글1',
-                'guard',
-                '563번답글컨텐트1',
-                563,
-                2,
-                1
-              );
-commit;              
-  --1번step
-UPDATE board SET step = step + 1 WHERE step >= 2 AND groupno = 563;
-commit;
+
+
+
+
 /*
  * 2. 
  * 	  boardno:시퀀스증가
@@ -80,41 +74,15 @@ commit;
  *    depth  :원글의 depth+1
  *    
  */             
-INSERT INTO board(boardno,title,writer,content, groupno, step, depth) 
-               VALUES
-            (board_sequence.nextval,
-                '563번답글2',
-                'guard',
-                '563번답글컨텐트2',
-                563,
-                2,
-                1
-              );              
-commit;  
+
+
+
+
 
 --2번step
-UPDATE board SET step = step + 1 WHERE step > 2 AND groupno = 563 ;
 
-commit;
-/*
- * 2. 
- * 	  boardno:시퀀스증가
- *    groupno:원글의 groupno
- *    step   :원글의 step+1
- *    depth  :원글의 depth+1
- *    
- */
-INSERT INTO board(boardno,title,writer,content, groupno, step, depth) 
-               VALUES
-            (board_sequence.nextval,
-                '563번답글답글',
-                'guard',
-                '563번답글답글컨텐트',
-                563,
-                3,
-                2
-              );
-commit;  
+
+
 
 
 
--- user_product_cart_order/src/main/webapp/user_modify_form.jsp
+++ user_product_cart_order/src/main/webapp/user_modify_form.jsp
@@ -68,16 +68,13 @@
 								<table border="0" cellpadding="0" cellspacing="1" width="590"
 									bgcolor="BBBBBB">
 									<tr>
-										<td width=100 align=center bgcolor="E6ECDE" height="22">사용자
-											아이디</td>
-										<td width=490 bgcolor="ffffff" style="padding-left: 10px"
-											align="left"><%=user.getUserId()%></td>
+										<td width=100 align=center bgcolor="E6ECDE" height="22">사용자아이디</td>
+										<td width=490 bgcolor="ffffff" style="padding-left: 10px"align="left"><%=user.getUserId()%></td>
 									</tr>
 									<tr>
 										<td width=100 align=center bgcolor="E6ECDE" height="22">비밀번호</td>
-										<td width=490 bgcolor="ffffff" style="padding-left: 10px"
-											align="left"><input type="password" style="width: 150px"
-											name="password" value="<%=user.getPassword()%>"></td>
+										<td width=490 bgcolor="ffffff" style="padding-left: 10px" align="left">
+											<input type="password" style="width: 150px" name="password" value="<%=user.getPassword()%>"></td>
 									</tr>
 									<tr>
 										<td width=100 align=center bgcolor="E6ECDE" height="22">비밀번호
@@ -89,8 +86,8 @@
 									<tr>
 										<td width=100 align=center bgcolor="E6ECDE" height="22">이름</td>
 										<td width=490 bgcolor="ffffff" style="padding-left: 10px"
-											align="left"><input type="text" style="width: 150px"
-											name="name" value="<%=user.getName()%>"></td>
+											align="left">
+											<input type="text" style="width: 150px" name="name" value="<%=user.getName()%>"></td>
 									</tr>
 									<tr>
 										<td width=100 align=center bgcolor="E6ECDE" height="22">이메일
